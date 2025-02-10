f9842edf7ce3992eef3311b9e607cfa483bbc74f
Gueudid <123460875+Gueudid@users.noreply.github.com>
2025-02-10 04:16:17 +0300

    Delete js directory

--- js/settings.js
+++
@@ -1,9 +0,0 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('darkMode', document.body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
-}
-
-// Проверяем сохраненные настройки
-if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-}
