bce7cfff4d3585820a3716cd697edf20a24d5112
Gueudid <123460875+Gueudid@users.noreply.github.com>
2025-02-11 22:30:14 +0000

    Deploying to gh-pages from @ Gueudid/GRHapp@08a1fe716fed73bbc3222df43bdaf7f527553474 🚀

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Загруженные данные:", data);
+        let id = decodeURIComponent(new URLSearchParams(window.location.search).get("id"));
+        console.log("Искомый ID:", id);
+        let found = data.apps.find(app => app.name.trim().toLowerCase() === id.trim().toLowerCase());
+        if (found) {
+            console.log("Найденное приложение:", found);
+        } else {
+            console.error("Приложение не найдено в data.json");
+            
+    .catch(error => console.error("Ошибка загрузки data.json:", error));
 document.addEventListener("DOMContentLoaded", function () {
     fetch("data.json")
         .then(response => {
